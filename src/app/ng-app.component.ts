6ba9784d4e4d9b80da16ab20fc17ce88b1039327
James Traggianese <james@hubspot.com>
2016-05-11 22:21:15 -0400

    Add Navbar component

--- src/app/ng-app.component.ts
+++ src/app/ng-app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from './navbar/navbar.component';
+
 
 @Component({
   moduleId: module.id,
   selector: 'ng-app-app',
+  directives: [NavbarComponent],
   templateUrl: 'ng-app.component.html',
   styleUrls: ['ng-app.component.css']
 })
 export class NgAppAppComponent {
-  title = 'ng-app works!';
+  
 }
