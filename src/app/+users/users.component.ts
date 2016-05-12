141d6bd7705e23044e90c0e57f0c3e0e05e1ec3c
James Traggianese <james@hubspot.com>
2016-05-12 10:48:21 -0400

    Add users to service and table

--- src/app/+users/users.component.ts
+++ src/app/+users/users.component.ts
@@ -1,18 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
+import { UsersService } from './users.service';
 
 @Component({
   moduleId: module.id,
   selector: 'app-users',
+  providers: [UsersService],
   templateUrl: 'users.component.html',
   styleUrls: ['users.component.css']
 })
 export class UsersComponent implements OnInit {
-
-  constructor() {}
+  users: any[];
+  constructor(private _service: UsersService) {}
 
   ngOnInit() {
-   
+    this._service.getUsers()
+      .subscribe(users => this.users = users);
+    
   }
 
 }
--- src/app/+users/users.service.ts
+++ src/app/+users/users.service.ts
@@ -1,13 +1,18 @@
 import { Injectable } from '@angular/core'
 import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 
-export class UserService {
+export class UsersService {
+  private _url = 'http://jsonplaceholder.typicode.com/users';
+  constructor(private _http: Http) {
+   
+  }
   
-  constructor() {
-    
-    
+  getUsers(){
+    return this._http.get(this._url)
+            .map(res => res.json());
   }
   
 }
