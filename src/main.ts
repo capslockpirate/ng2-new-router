8588513ad11ed3fc8c620b1c994cd4c3721b8a0e
James Traggianese <james@hubspot.com>
2016-05-12 10:03:17 -0400

    Add router and http providers to bootfile

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,12 @@ import { bootstrap } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { NgAppAppComponent, environment } from './app/';
 
+import { ROUTER_PROVIDERS } from '@angular/router';
+import { HTTP_PROVIDERS } from '@angular/http';
+
+
 if (environment.production) {
   enableProdMode();
 }
 
-bootstrap(NgAppAppComponent);
+bootstrap(NgAppAppComponent, [ROUTER_PROVIDERS, HTTP_PROVIDERS]);
