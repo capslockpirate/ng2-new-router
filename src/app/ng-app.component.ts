f0604d529fc5fd42688bf4f24f8a57d20d1fcd53
James Traggianese <james@hubspot.com>
2016-05-12 10:02:46 -0400

    Add router and router outlet

--- src/app/ng-app.component.ts
+++ src/app/ng-app.component.ts
@@ -1,14 +1,26 @@
 import { Component } from '@angular/core';
+import { Location } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './+home';
+import { Routes, Router, ROUTER_DIRECTIVES, ROUTER_PROVIDERS} from '@angular/router';
+import { UsersComponent } from './+users';
 
 
 @Component({
   moduleId: module.id,
   selector: 'ng-app-app',
-  directives: [NavbarComponent],
+  directives: [NavbarComponent, ROUTER_DIRECTIVES],
   templateUrl: 'ng-app.component.html',
-  styleUrls: ['ng-app.component.css']
+  styleUrls: ['ng-app.component.css'],
+  providers: [ROUTER_PROVIDERS]
 })
+@Routes([
+  {path: '/', component: HomeComponent},
+  {path: '/home', component: HomeComponent},
+  {path: '/users', component: UsersComponent}
+])
 export class NgAppAppComponent {
-  
+  title = 'ng-app works!';
+
+
 }
